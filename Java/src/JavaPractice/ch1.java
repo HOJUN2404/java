e18bc2e94b53619bb2e15b30db0fadbe3e9950f7
HOJUN2404 <rlaghwns1995@naver.com>
2021-10-12 21:26:19 +0900

    면접 전 for문과 배열 복습해보기

---
+++ Java/src/JavaPractice/ch1.java
@@ -0,0 +1,26 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class ch1 {
+
+	public static void main(String[] args) {
+		// 면접 전 for문 연습하기
+		System.out.println("숫자 n을 입력하세요 : ");
+		
+		int n = 0;
+		Scanner scanner = new Scanner(System.in);
+		n = scanner.nextInt();
+
+		for(int i=1 ; i<=n ; i++)
+		{
+			System.out.println(i);
+		}
+	}
+	
+	
+	public static void ex2(String[] args) {
+		
+	}
+
+}
