26c74453c93a0b20f1438175295571c62d71e64c
HOJUN2404 <rlaghwns1995@naver.com>
2021-10-21 23:31:28 +0900

    백준 [함수] 마무리

--- Java/src/baekJoon/Ex11_1065.java
+++ Java/src/baekJoon/Ex11_1065.java
@@ -28,7 +28,7 @@ public class Ex11_1065 {
 			for(int i = 100 ; i <= n ; i++)
 			{
 				int hun = i / 100;
-				int ten = i / 10;
+				int ten = (i / 10) % 10;
 				int one = i % 10;
 				
 				if((hun - ten) == (ten - one))
