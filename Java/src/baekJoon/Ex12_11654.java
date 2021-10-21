5fbf0dc94c07540aada2190ae50bd8779473ea69
HOJUN2404 <rlaghwns1995@naver.com>
2021-10-21 23:36:35 +0900

    백준 아스키코드 문제풀이

---
+++ Java/src/baekJoon/Ex12_11654.java
@@ -0,0 +1,20 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class Ex12_11654 {
+
+	public static void main(String[] args) {
+		// 아스키 코드값 출력
+		
+		Scanner scn = new Scanner(System.in);
+		
+		char a = scn.nextLine().charAt(0);
+		
+		int num = (int)a;
+		
+		System.out.println(num);
+
+	}
+
+}
