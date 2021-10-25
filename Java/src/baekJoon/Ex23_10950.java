a64187c01cd0526e05dd028b67ebd0b280fea585
HOJUN2404 <rlaghwns1995@naver.com>
2021-10-25 23:48:29 +0900

    10.25 백준 풀이

---
+++ Java/src/baekJoon/Ex23_10950.java
@@ -0,0 +1,24 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class Ex23_10950 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		int a = scn.nextInt();
+		
+		for(int i = 0 ; i < a ; i++)
+		{
+			int b = scn.nextInt();
+			int c = scn.nextInt();
+			
+			System.out.println(b + c);
+					
+		}
+				
+
+	}
+
+}
