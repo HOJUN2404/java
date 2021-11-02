cf2f3d2c6e1200e3191d858deab6a445889c9451
HOJUN2404 <rlaghwns1995@naver.com>
2021-11-02 17:57:25 +0900

    백준 알고리즘 풀이

---
+++ Java/src/baekJoon/Ex37_10951.java
@@ -0,0 +1,22 @@
+package baekJoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex37_10951 {
+
+public static void main(String args[]) throws IOException {
+		
+	Scanner in=new Scanner(System.in);
+	
+	while(in.hasNextInt()){
+	
+		int a=in.nextInt();
+		int b=in.nextInt();
+		System.out.println(a+b);
+	
+	}	
+	in.close();
+	}
+
+}
