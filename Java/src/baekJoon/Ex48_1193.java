6e92d1bb11a450dfca65f24d0556e55483f4dd2b
HOJUN2404 <rlaghwns1995@naver.com>
2021-11-14 17:21:41 +0900

    백준[자바] 1193번 문제풀이

---
+++ Java/src/baekJoon/Ex48_1193.java
@@ -0,0 +1,26 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class Ex48_1193 {
+
+	public static void main(String[] args) {
+		// 백준 1193번 분수찾기 문제
+		
+		Scanner scn = new Scanner(System.in);
+		
+		int x = scn.nextInt();
+		
+		int cross_count = 1;
+		int prevCountSum = 0;
+		
+		while (true)
+		{
+			if(x <= prevCountSum + cross_count)
+			{
+				
+			}
+		}
+	}
+
+}
