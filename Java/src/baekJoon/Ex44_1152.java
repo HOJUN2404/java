f93c3937f47c78979056dc54fa5095ba179a2c2f
HOJUN2404 <rlaghwns1995@naver.com>
2021-11-24 22:23:23 +0900

    백준 1152번 문제풀이

---
+++ Java/src/baekJoon/Ex44_1152.java
@@ -0,0 +1,39 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex44_1152 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+//		String a = br.readLine();
+//		
+//		StringTokenizer st;
+//		
+//		st = new StringTokenizer(a, " ");
+//		
+//		System.out.println(st.countTokens());
+		
+		
+		
+//		int num = 5;
+//		
+//		char c = Character.forDigit(num, 10);
+//		
+//		System.out.println(c);
+		
+		
+		char[] cArr = {'김', '호', '준', '입', '니', '다'};
+		String str = new String(cArr, 1, 4);
+		System.out.println(str);
+		
+		
+
+
+	}
+
+}
